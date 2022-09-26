15a4ef8f7a119cf5052aaefb183e2d0cac7ab0c7
cristian <cristianengel1411@gmail.com>
2022-09-26 18:26:10 -0300

    Creacion de los atributos de la clase Biblioteca

--- src/main/java/Biblioteca.java
+++ src/main/java/Biblioteca.java
@@ -1,5 +1,14 @@
 import java.util.ArrayList;
 
 public class Biblioteca {
+    ArrayList<Obra> listaDeObras = new ArrayList<>();
+    ArrayList<String> listaDeIndices = new ArrayList<>();
+    ArrayList<Lector> listaDeDeudores = new ArrayList<>();
+    ArrayList<Obra> listaObrasSolicitadasAluDoc = new ArrayList<>();
+    ArrayList<Obra> listaObrasSolicitadasPublico = new ArrayList<>();
+    ArrayList<Lector> listaLectoresConMultas = new ArrayList<>();
 
+    public void agregarEjemplar(Ejemplar ejemplar){
+        // TODO: 9/26/22  
+    }
 }
