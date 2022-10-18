ed587e1611be8378152d571e2f7d7e633a041211
cristian <cristianengel1411@gmail.com>
2022-10-18 11:31:11 -0300

    - Eliminé el atributo areaDeReferencia de la clase Ejemplar (innecesario) - Avances en la UI (pantalla de adquisición de ejemplares)

--- src/main/java/MainClasses/Ejemplar.java
+++ src/main/java/MainClasses/Ejemplar.java
@@ -13,7 +13,6 @@ public class Ejemplar {
     private String identUnico;
     private int codBarra;
     private String observaciones;
-    private String areaDeReferencia;
     private ArrayList<String> lugarFisico = new ArrayList<>();
 
     private LocalDate fechaDadoDeBaja;
@@ -28,26 +27,24 @@ public class Ejemplar {
     private Prestamo lectorPrestamo; //Relación con las clases Prestamo y Lector
 
     //Constructor sin reserva ni préstamo
-    public Ejemplar(String identUnico, int codBarra, String observaciones, String areaDeReferencia, ArrayList<String> lugarFisico, LocalDate fechaDadoDeBaja, String motivoDadoDeBaja, ArrayList<String> ubicacionFisicaDadoDeBaja, LocalDate fechaDeAdquisicion, String formaDeAdquisicion, Obra obra) {
+    public Ejemplar(String identUnico, int codBarra, String observaciones, ArrayList<String> lugarFisico, LocalDate fechaDadoDeBaja, String motivoDadoDeBaja, ArrayList<String> ubicacionFisicaDadoDeBaja, LocalDate fechaDeAdquisicion, String formaDeAdquisicion, Obra obra) {
         this.identUnico = identUnico;
         this.codBarra = codBarra;
-        this.observaciones = observaciones;
-        this.areaDeReferencia = areaDeReferencia;
         this.lugarFisico = lugarFisico;
         this.fechaDadoDeBaja = fechaDadoDeBaja;
         this.motivoDadoDeBaja = motivoDadoDeBaja;
         this.ubicacionFisicaDadoDeBaja = ubicacionFisicaDadoDeBaja;
         this.fechaDeAdquisicion = fechaDeAdquisicion;
         this.formaDeAdquisicion = formaDeAdquisicion;
+        this.observaciones = observaciones;
         this.obra = obra;
     }
 
     //Constructor con reserva
-    public Ejemplar(String identUnico, int codBarra, String observaciones, String areaDeReferencia, LocalDate fechaDadoDeBaja, String motivoDadoDeBaja, ArrayList<String> ubicacionFisicaDadoDeBaja, LocalDate fechaDeAdquisicion, String formaDeAdquisicion, Obra obra, Reserva lectorReserva, Prestamo lectorPrestamo) {
+    public Ejemplar(String identUnico, int codBarra, String observaciones, LocalDate fechaDadoDeBaja, String motivoDadoDeBaja, ArrayList<String> ubicacionFisicaDadoDeBaja, LocalDate fechaDeAdquisicion, String formaDeAdquisicion, Obra obra, Reserva lectorReserva, Prestamo lectorPrestamo) {
         this.identUnico = identUnico;
         this.codBarra = codBarra;
         this.observaciones = observaciones;
-        this.areaDeReferencia = areaDeReferencia;
         this.fechaDadoDeBaja = fechaDadoDeBaja;
         this.motivoDadoDeBaja = motivoDadoDeBaja;
         this.ubicacionFisicaDadoDeBaja = ubicacionFisicaDadoDeBaja;
@@ -58,11 +55,10 @@ public class Ejemplar {
     }
 
     //Constructor con préstamo
-    public Ejemplar(String identUnico, int codBarra, String observaciones, String areaDeReferencia, ArrayList<String> lugarFisico, LocalDate fechaDadoDeBaja, String motivoDadoDeBaja, ArrayList<String> ubicacionFisicaDadoDeBaja, LocalDate fechaDeAdquisicion, String formaDeAdquisicion, Obra obra, Prestamo lectorPrestamo) {
+    public Ejemplar(String identUnico, int codBarra, String observaciones, ArrayList<String> lugarFisico, LocalDate fechaDadoDeBaja, String motivoDadoDeBaja, ArrayList<String> ubicacionFisicaDadoDeBaja, LocalDate fechaDeAdquisicion, String formaDeAdquisicion, Obra obra, Prestamo lectorPrestamo) {
         this.identUnico = identUnico;
         this.codBarra = codBarra;
         this.observaciones = observaciones;
-        this.areaDeReferencia = areaDeReferencia;
         this.lugarFisico = lugarFisico;
         this.fechaDadoDeBaja = fechaDadoDeBaja;
         this.motivoDadoDeBaja = motivoDadoDeBaja;
@@ -77,7 +73,7 @@ public class Ejemplar {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Ejemplar ejemplar)) return false;
-        return codBarra == ejemplar.codBarra && Objects.equals(identUnico, ejemplar.identUnico) && Objects.equals(observaciones, ejemplar.observaciones) && Objects.equals(areaDeReferencia, ejemplar.areaDeReferencia) && Objects.equals(fechaDadoDeBaja, ejemplar.fechaDadoDeBaja) && Objects.equals(motivoDadoDeBaja, ejemplar.motivoDadoDeBaja) && Objects.equals(ubicacionFisicaDadoDeBaja, ejemplar.ubicacionFisicaDadoDeBaja) && Objects.equals(fechaDeAdquisicion, ejemplar.fechaDeAdquisicion) && Objects.equals(formaDeAdquisicion, ejemplar.formaDeAdquisicion) && Objects.equals(obra, ejemplar.obra) && Objects.equals(lectorReserva, ejemplar.lectorReserva) && Objects.equals(lectorPrestamo, ejemplar.lectorPrestamo);
+        return codBarra == ejemplar.codBarra && Objects.equals(identUnico, ejemplar.identUnico) && Objects.equals(observaciones, ejemplar.observaciones) &&  Objects.equals(fechaDadoDeBaja, ejemplar.fechaDadoDeBaja) && Objects.equals(motivoDadoDeBaja, ejemplar.motivoDadoDeBaja) && Objects.equals(ubicacionFisicaDadoDeBaja, ejemplar.ubicacionFisicaDadoDeBaja) && Objects.equals(fechaDeAdquisicion, ejemplar.fechaDeAdquisicion) && Objects.equals(formaDeAdquisicion, ejemplar.formaDeAdquisicion) && Objects.equals(obra, ejemplar.obra) && Objects.equals(lectorReserva, ejemplar.lectorReserva) && Objects.equals(lectorPrestamo, ejemplar.lectorPrestamo);
     }
 
     @Override
@@ -86,7 +82,6 @@ public class Ejemplar {
                 "Identificador único='" + identUnico + '\'' +
                 ", Código de barra=" + codBarra +
                 ", Observaciones='" + observaciones + '\'' +
-                ", Área de referencia='" + areaDeReferencia + '\'' +
                 ", Fecha de dado de baja=" + fechaDadoDeBaja +
                 ", Motivo de dado de baja='" + motivoDadoDeBaja + '\'' +
                 ", Ubicación física de dado de baja='" + ubicacionFisicaDadoDeBaja + '\'' +
