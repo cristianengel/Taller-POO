29ade2d337dda487a203d771d8adf3d2646c826c
Isa <isareniero936@gmail.com>
2022-10-21 16:22:22 -0300

    Se agrega documentación a las clases del paquete Enum.

--- src/main/java/Enum/Sexo.java
+++ src/main/java/Enum/Sexo.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 
 /**
- * La clase TipoSexo ofrece diferenciar tres tipos de identidad de genero
+ * La clase TipoSexo ofrece diferenciar tres tipos de identidad de género.
  */
 public enum Sexo {
     /* Sexo masculino */
@@ -18,7 +18,8 @@ public enum Sexo {
     private String sexo;
 
     /**
-     * Constructor privado, para que solamente se creen objetos del tipo mencionados aquí
+     * Constructor de la clase Sexo privado
+     * para que solamente se creen objetos del tipo mencionados aquí.
      * @param sexo
      */
     private Sexo(String sexo) {
--- src/main/java/Enum/TipoLectura.java
+++ src/main/java/Enum/TipoLectura.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 @Getter
 
 /**
- * Clase en la cuál se proponen los tipos de lectura que se pueden realizan
- * al momento de realizar un préstamo
+ * La clase TipoLectura propone los tipos de lectura que se pueden registrar
+ * al momento de realizar un préstamo.
  */
 public enum TipoLectura {
     /* Tipo de lectura a domicilio al solicitar el préstamo de un ejemplar */
@@ -17,7 +17,8 @@ public enum TipoLectura {
     private final String tipo;
 
     /**
-     * Constructor privado, para que solamente se creen objetos del tipo mencionados aquí
+     * Constructor de la clase TipoLectura privado
+     * para que solamente se creen objetos del tipo mencionados aquí.
      * @param tipo
      */
     private TipoLectura(String tipo) {
